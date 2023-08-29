8e24f964d75417d18462dfdc33d9358f4ddb545b
Brane105 <brane6963@gmail.com>
2023-08-30 00:10:30 +0530

    google auth completed

--- src/components/GAuth.jsx
+++ src/components/GAuth.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
-import {FcGoogle} from 'react-icons/fc'
+import {GoogleAuthProvider, getAuth, signInWithPopup} from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc} from "firebase/firestore"
+import {db} from "../firebase"
+import {FcGoogle} from 'react-icons/fc';
+import {toast} from "react-toastify";
+import {useNavigate} from "react-router-dom"
 export default function GAuth() {
+  const navigate =  useNavigate()
+  async function onGoogleClick(){
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth,provider);
+      const user = result.user;
+      console.log(user);
+      //checking if the user exists
+      const docRef = doc(db,"users",user.uid);
+      const docSnap = await getDoc(docRef);
+      console.log(docSnap);
+      if(!docSnap.exists()){
+        await setDoc(docRef,{
+          name :user.displayName,
+          email :user.email,
+          timestamp: serverTimestamp()
+        });
+      }
+      navigate('/');
+    } catch (error) {
+      console.log(error)
+      toast.error('Something went wrong !')
+    }
+  }
   return (
-    <button className='flex
+    <button type='button' onClick={onGoogleClick} className='flex
     items-center justify-center 
     w-full bg-red-600
     text-white px-7 py-3 uppercase
